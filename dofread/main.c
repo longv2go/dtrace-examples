ded3bda5c5e826e4b1ead4e5d4074b264c477f2c
longv <longv2j@gmail.com>
2018-01-22 14:06:23 +0800

    usdt test

---
+++ dofread/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  dofread
+//
+//  Created by albert on 2018/1/18.
+//  Copyright © 2018年 ant. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
